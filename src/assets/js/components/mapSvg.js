878c4f165b3bfc62af22d50958a386c69177400c
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-08-02 23:58:47 -0500

    para probar en heroku

---
+++ src/assets/js/components/mapSvg.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const MapaSvg = (update) => {
+  const section = $('<section class="map-svg"></section>');
+
+  var svg = `<svg xmlns="http://www.w3.org/2000/svg" version='1.1' width="100%" height="100%" >`;
+<line id="" x1='700' y1="400" x2='700' y2='340' style='stroke:blue;stroke-width:5'/>
+};
