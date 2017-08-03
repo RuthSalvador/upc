7fbfce151c23189c3f569df4685af985fcedebc3
FiorellaQA <fiorella_90_17@hotmail.com>
2017-08-03 02:28:47 -0500

    Añadiendo funcionalidad segun sede

--- src/assets/js/components/sedes.js
+++ src/assets/js/components/sedes.js
@@ -13,41 +13,23 @@ const Sedes = () => {
 	const villa = $('<div class="sede--Villa col-xs-12 col-sm-6"><p>Campus Villa</p></div>');
 	const sMiguel = $('<div class="sede--SanMiguel col-xs-12 col-sm-6"><p>Campus San Miguel</p></div>');
 
-/*
-	btn.on('click',function (e) {
-		e.preventDefault();
 
-		$.post('/api/registerNumber',{
-			phone: input.val(),
-			terms: $("#terminos").is(':checked')
-		},function(response){
-			if (response.success) {
-				state.screen = "pantalla3";
-				state.phone = response.data.phone;
-				state.code = response.data.code;
-				console.log(response.success);
-				console.log('Codigo: ' + response.data.code);
-				update();
-			} else {
-				state.screen = "pantalla2";
-				message.text("response.message");
-				console.log(response.message);
-				update();
-			}
-		});
-	});
-	*/
 
 	monterrico.on('click',(e) => {
+		e.preventDefault();
 		getJSON('/rutasMo', (err, json) => {
 			state.rutasMo = json;
 			console.log(json.features);
+			$.each(json.features, ( key, value ) =>  {
+				console.log(value);
+			};
 		});
+			/*
 		getJSON('/upcMonterrico', (err, json) => {
 			state.rutasMo = json;
 			console.log(json.features);
-		});
-		e.preventDefault();
+		});*/
+
 		//state.page = 2;
 		//update();
 	});
